603e3c2b0414d0f2eed5b00fbbd23f2d63fdb37f
toalt23 <to.alt@web.de>
2026-08-18 15:13:07 +0000

    web-ui polling inscreased to stablize

--- toalt23-zec-mining-pool/web-ui/src/node/node.service.ts
+++ toalt23-zec-mining-pool/web-ui/src/node/node.service.ts
@@ -41,7 +41,7 @@ export class NodeService {
                 },
                 {
                     headers: { 'Content-Type': 'application/json' },
-                    timeout: 60000,
+                    timeout: 30000,
                 },
             );
 
